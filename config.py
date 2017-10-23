645c289e0518f41ac9428f638ed7d52c5a294096
DhirajBhakta <dhirajbhakta110@gmail.com>
2017-10-24 02:07:58 +0530

    feature: add new drug to inventory

--- config.py
+++ config.py
@@ -4,20 +4,20 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = '123456789987654321'
-    
+
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
     MAIL_USE_SSL = True
     MAIL_USE_TLS = False
     MAIL_USERNAME = 'nitkhcc@gmail.com'
     MAIL_PASSWORD = 'nitkhccnitkhcc'
-    
+
     HCC_MAIL_SUBJECT_PREFIX = '[NITK-Health Care Center]'
     HCC_MAIL_SENDER = 'nitkhcc@gmail.com'
     HCC_ADMIN = "fill this shit later"
-    
-    MYSQL_DATABASE_USER = 'root'
-    MYSQL_DATABASE_PASSWORD = 'student'
+
+    MYSQL_DATABASE_USER = 'mysql'
+    MYSQL_DATABASE_PASSWORD = 'mysql'
     MYSQL_DATABASE_DB = 'HCCdb'
     MYSQL_DATABASE_HOST = 'localhost'
 
@@ -28,7 +28,7 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    
+
 
 class TestingConfig(Config):
     TESTING = True
